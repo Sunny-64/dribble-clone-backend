4ce388ce162635d39b13d8e7f6b3c9be34aca268
Sunny <sunny6464n@gmail.com>
2024-05-01 10:22:33 +0530

    allow preflight to app

--- src/index.js
+++ src/index.js
@@ -1,52 +1,63 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
-// library imports 
-const express = require('express'); 
-const cors = require("cors"); 
-const morgan = require("morgan")
+// library imports
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-// custom imports 
-const initiateServer = require('./config/server'); 
+// custom imports
+const initiateServer = require("./config/server");
 
+const app = express();
+const router = express.Router();
+initiateServer(app);
 
-const app = express(); 
-const router = express.Router(); 
-initiateServer(app); 
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
-app.use(cors({
-    origin : '*',
-}))
-app.use(morgan('dev')); 
-app.use(express.json()); 
-app.use(express.urlencoded({extended : true})); 
-
-
-app.use("/.netlify/functions/app", router);
-
-router.options('*', cors()); // Enable preflight for all routes
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://dribble-clone-gamma.vercel.app/");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/.netlify/functions/app", router);
+
+// router.options("*", cors()); // Enable preflight for all routes
+// router.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
+
 router.get("/", (req, res) => {
-    res.status(200).json({message : "Welcome to dribble clone backend"}); 
-})
+    res.status(200).json({ message: "Welcome to dribble clone backend" });
+});
 
 // Auth routes...
-const authRoutes = require('./routes/auth'); 
-router.use('/auth', authRoutes); 
+const authRoutes = require("./routes/auth");
+router.use("/auth", authRoutes);
 
 // User routes...
-const userRoutes = require('./routes/user'); 
-router.use('/user', userRoutes); 
+const userRoutes = require("./routes/user");
+router.use("/user", userRoutes);
 
 // Invalid routes...
 router.get("*", (req, res) => {
-    res.status(404).json({message : 'not-found'}); 
-})
+    res.status(404).json({ message: "not-found" });
+});
 
 module.exports = {
     app,
-}
+};
