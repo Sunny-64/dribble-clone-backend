b80f0dd02a8f8d11de087a98bbeccdb9b214e2dd
Sunny <sunny6464n@gmail.com>
2024-05-01 19:25:56 +0530

    fix bcrypt error

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcryptjs'); 
 
 const { sendMail } = require('../services/email');
 const User = require('./../models/user'); 
